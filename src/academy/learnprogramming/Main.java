63d4cea19fe255fc37b65dc46ce242dbc64a186c
myizci <izci@hotmail.com>
2020-11-10 12:49:12 -0500

    project is already in remote repo,

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         //New
+        //This is after publishing on GitHub.com
+        //Do I need to share again?
+        //Commit and Push!!
 
     }
 }
